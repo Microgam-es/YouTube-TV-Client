310d4d239dc6b7dbc4e1f336344be940c8df7e4c
Andreas Rittsel <67197485+Andreascoolcrafts@users.noreply.github.com>
2022-11-02 22:35:48 +0100

    added splash screen

---
+++ sourceCode/Xcode/youtubeTVClient/Shared/SplashScreenView.swift
@@ -0,0 +1,9 @@
+//
+//  SplashScreenView.swift
+//  YouTube TV Client (macOS)
+//
+//  Created by Andreas Rittsel on 02.11.22.
+//  Copyright © 2022 Microgam.es. All rights reserved.
+//
+
+import Foundation
